afbd722b4e94052f2b528e6009dbf093ffe5488e
Marc Altmann <marcaltmann@posteo.de>
2020-10-02 16:38:10 +0200

    More singleton class specs

--- chapter13/spec/singleton_classes_spec.rb
+++ chapter13/spec/singleton_classes_spec.rb
@@ -23,4 +23,36 @@ RSpec.describe 'singleton classes' do
 
     expect(Person.possible_names[0]).to eq('Schmidt')
   end
+
+  it 'ancestor exercise p. 424' do
+    module P
+    end
+
+    class Object
+      prepend P
+    end
+
+    module M
+    end
+
+    class C
+    end
+
+    obj = C.new
+
+    class << obj
+      prepend P
+      include M
+    end
+
+    actual = obj.singleton_class.ancestors
+    expected = [P, obj.singleton_class, M, C, P, Object, Kernel, BasicObject]
+    expect(actual).to eq(expected)
+  end
+
+  it 'singleton_class method' do
+    obj = Object.new
+
+    expect(obj.singleton_class.class.name).to eq('Class')
+  end
 end
